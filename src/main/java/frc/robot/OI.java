3a9a89ffbcc45de50d77a9359b497ae6db659c36
Casey <caseydowning@outlook.com>
2019-01-11 17:18:36 -0800

    Added tankdrive, OI, and Tank Drive with Xbox

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,36 +7,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  public static final int DRIVER_PORT = 0;
+  public static final double XBOX_LEFT_Y_THRESHOLD = 0.1;
+  public static final double XBOX_RIGHT_Y_THRESHOLD = 0.1;
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+  XboxController Driver = new XboxController(DRIVER_PORT);
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+  public double getDriverLeftYAxis() {
+    double rawLeftYAxis = Driver.getY(Hand.kLeft);
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+    return deadband(rawLeftYAxis, XBOX_LEFT_Y_THRESHOLD);
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+  }
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+  public double getDriverRightYAxis() {
+    double rawRightYAxis = Driver.getY(Hand.kRight);
+
+    return deadband(rawRightYAxis, XBOX_RIGHT_Y_THRESHOLD);
+
+  }
+
+  public double deadband(double input, double threshold) {
+    return Math.abs(input) <= Math.abs(threshold) ? 0 : input;
+  }
+  
 }
