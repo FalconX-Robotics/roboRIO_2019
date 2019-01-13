d5a377be27d6bc7cbca3e7fd3262ce8a925a269f
Minyoung Ahn <myahn725@gmail.com>
2019-01-12 20:16:24 -0800

    added tank drive, arcade drive method, create drivetrain object

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Drivetrain extends Subsystem {
 
@@ -14,6 +15,16 @@ public class Drivetrain extends Subsystem {
   private WPI_TalonSRX rightFront = new WPI_TalonSRX(2); //robotMap this later
   private WPI_TalonSRX rightRear = new WPI_TalonSRX(3);
   private SpeedControllerGroup rightSide = new SpeedControllerGroup(rightFront, rightRear);
+
+  DifferentialDrive drivetrain = new DifferentialDrive(leftSide, rightSide);
+
+  public void tankDrive(double leftSpeed, double rightSpeed){
+    drivetrain.tankDrive(leftSpeed, rightSpeed);
+  }
+
+  public void arcadeDrive(double speed, double rotation){
+    drivetrain.arcadeDrive(speed, rotation);
+  }
 
   protected void initDefaultCommand() {
     //setDefaultCommand(new TankDriveWithJava());
