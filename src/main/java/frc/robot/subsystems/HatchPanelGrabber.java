fa1aadfb92d3670db5d1904ba7d98b92f38f334a
Minyoung Ahn <myahn725@gmail.com>
2019-01-16 23:59:30 -0800

    deleted import DoubleSolenoid.Value because it wasn't used

--- src/main/java/frc/robot/subsystems/HatchPanelGrabber.java
+++ src/main/java/frc/robot/subsystems/HatchPanelGrabber.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
