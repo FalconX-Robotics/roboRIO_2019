c6e77a3b08abeb66841c0f0bf8a1a14c1c959557
Casey <caseydowning@outlook.com>
2019-01-21 09:54:11 -0800

    New and improved hatch panel coming soon to theaters near you

--- src/main/java/frc/robot/commands/LaunchPanel.java
+++ src/main/java/frc/robot/commands/LaunchPanel.java
@@ -1,48 +1,43 @@
 package frc.robot.commands;
 
-import java.util.concurrent.TimeUnit;
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class LaunchPanel extends Command {
-   private static DoubleSolenoid.Value out = DoubleSolenoid.Value.kForward;
-   private static DoubleSolenoid.Value in = DoubleSolenoid.Value.kReverse;
-   private boolean finished = false;
+public class LaunchPanel extends CommandGroup {
+    private static DoubleSolenoid.Value out = DoubleSolenoid.Value.kForward;
+    private static DoubleSolenoid.Value in = DoubleSolenoid.Value.kReverse;
 
-   public LaunchPanel() {
-       super("LaunchPanelYourMom");
-       //Grab solenoid in
-       //addSequential(new ToggleHatchGrabSolenoid(in));
-       //Wait 0.5
-       //addSequential(new WaitCommand(), 0.5);
-       //Push solenoid out
-       //addSequential(new ToggleHatchPushSolenoid(out));
-       //Wait 0.5
-       //addSequential(new WaitCommand(), 0.5);
-       //Push solenoid In
-       //addSequential(new ToggleHatchPushSolenoid(in));
-   }
-
-   @Override
-   protected void initialize() {
-        try {
-            Robot.hatchPanelGrabber.toggleHatchGrabSolenoid(out);
-            TimeUnit.MILLISECONDS.sleep(300);
-            Robot.hatchPanelGrabber.toggleHatchPushSolenoid(out);
-            TimeUnit.MILLISECONDS.sleep(100);
-            Robot.hatchPanelGrabber.toggleHatchPushSolenoid(in);
-        } catch (InterruptedException e) {
-
-        }
-        finished = true;
-   }
-
-    @Override
-    protected boolean isFinished() {
-        return finished;
+    public LaunchPanel() {
+        super("LaunchPanelYourMom");
+        // Grab solenoid in
+        addSequential(new ToggleHatchGrabSolenoid(out));
+        // Wait 0.5
+        addSequential(new WaitCommand(), 0.3);
+        // Push solenoid out
+        addSequential(new ToggleHatchPushSolenoid(out));
+        // Wait 0.5
+        addSequential(new WaitCommand(), 0.1);
+        // Push solenoid In
+        addSequential(new ToggleHatchPushSolenoid(in));
     }
 
+    @Override
+    protected void initialize() {
+        // try {
+        // Robot.hatchPanelGrabber.toggleHatchGrabSolenoid(out);
+        // TimeUnit.MILLISECONDS.sleep(300);
+        // Robot.hatchPanelGrabber.toggleHatchPushSolenoid(out);
+        // TimeUnit.MILLISECONDS.sleep(100);
+        // Robot.hatchPanelGrabber.toggleHatchPushSolenoid(in);
+        // } catch (InterruptedException e) {
+
+        // }
+        // finished = true;
+    }
+
+    // @Override
+    // protected boolean isFinished() {
+    // return finished;
+    // }
 
 }
---
+++ src/main/java/frc/robot/commands/ToggleHatchPushSolenoid.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import frc.robot.Robot;
+
+public class ToggleHatchPushSolenoid extends InstantCommand {
+    private Value toggleValue;
+
+    public ToggleHatchPushSolenoid(Value toggleValue) {
+        super("ToggleHatchPushSolenoid");
+        this.toggleValue = toggleValue;
+    }
+
+    public void initialize() {
+        Robot.hatchPanelGrabber.toggleHatchPushSolenoid(toggleValue);
+    }
+}
