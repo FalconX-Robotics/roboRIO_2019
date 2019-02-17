0b78c3c8db151ab61a300ec86fa1252d9d127171
Casey <caseydowning@outlook.com>
2019-02-17 10:25:12 -0800

    corrected comments in oi

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -34,7 +34,7 @@ public class OI {
 
   private static final int CLIMBER_INITIALIZE_BUTTON = 8; // Xbox Start button
   private static final int CLIMBER_FRONT_BUTTON = 3; // Xbox X button
-  
+
   private static final int CLIMBER_BACK_BUTTON = 4; // Xbox Y button
   // private static final int CLIMBER_BUTTON = 8;
 
@@ -47,8 +47,8 @@ public class OI {
   private static final int TOGGLE_HATCH_PANEL_BUTTON = 4; // Xbox X button
   private static final int LAUNCH_HATCH_PANEL_BUTTON = 3; // Xbox Y button
 
-  public static final int MOVE_HATCH_UP = 6; // Xbox start button
-  public static final int MOVE_HATCH_DOWN = 5;
+  public static final int MOVE_HATCH_UP = 6; // Xbox right bumper
+  public static final int MOVE_HATCH_DOWN = 5; // Xbox left bumper
 
   XboxController driverOne = new XboxController(DRIVER_PORT_ONE);
   XboxController driverTwo = new XboxController(DRIVER_PORT_TWO);
