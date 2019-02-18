a75d51dbf9b9ed884c26c10da289af0b6f5c3b65
Casey <caseydowning@outlook.com>
2019-02-18 15:53:47 -0800

    fixed robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -15,11 +15,11 @@ package frc.robot;
  */
 public class RobotMap {
   // Drivetrain
-  public static final int FRONT_LEFT_MOTOR = 1;
-  public static final int REAR_LEFT_MOTOR = 4;
+  public static final int FRONT_LEFT_MOTOR = 3;
+  public static final int REAR_LEFT_MOTOR = 2;
 
   public static final int FRONT_RIGHT_MOTOR = 5;
-  public static final int REAR_RIGHT_MOTOR = 3;
+  public static final int REAR_RIGHT_MOTOR = 4;
 
   // Gear shift
   public static final boolean ENABLE_TOGGLE_GEAR_SHIFT = true;
@@ -33,9 +33,9 @@ public class RobotMap {
 
   public static final int HATCH_PUSH = 2;
 
-  public static final int HATCH_MOTOR = 2; 
-  public static final int TOP_LIMIT_SWITCH = 0;
-  public static final int BOTTOM_LIMIT_SWITCH = 1;
+  public static final int HATCH_MOTOR = 1; 
+  public static final int TOP_LIMIT_SWITCH = 9;
+  public static final int BOTTOM_LIMIT_SWITCH = 8;
 
   // Climb
   public static final int FRONT_FORWARD_CLIMB_SOLENOID = 5;
@@ -44,15 +44,15 @@ public class RobotMap {
   public static final int BACK_FORWARD_CLIMB_SOLENOID = 5;
   public static final int BACK_REVERSE_CLIMB_SOLENOID = 4;
 
+  public static final int CLIMBER_MOTOR = 6;
+
   // Cargo
   public static final int CARGO_UPPER_PISTON_IN = 3;
   public static final int CARGO_UPPER_PISTON_OUT = 2;
 
   public static final int CARGO_LOWER_PISTON_IN = 6;
   public static final int CARGO_LOWER_PISTON_OUT = 7;
-
-  public static final int CLIMBER_MOTOR = 6;
-
+  
   // Encoders
   public static final int LEFT_ENCODER_CHANNEL_A = 0;
   public static final int LEFT_ENCODER_CHANNEL_B = 1;
